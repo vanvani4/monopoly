4a66cf164c1da1af3afb478d0b30010cf76a9f75
vanvani4 <vanvani4@rambler.ru>
2016-12-25 23:42:44 +0200

    Обновление

--- monopoly.py
+++ monopoly.py
@@ -1,29 +1,37 @@
 import gamer
+from msvcrt import getch
 player1 = gamer.Gamer('Alexey')
+player2 = gamer.Gamer('Ivan')
+player_list = [player1, player2]
+
+def is_winner_defined():
+    print('is_winner_defined(): This function checks whether the winner is found')
+
+def greetings_to_winner():
+    print('greetings _to_winner(): This function send greetings to the game winner!')
+
+def get_player_for_next_turn():
+    global player_list
+    active_player = player_list.pop(0)
+    player_list.append(active_player)
+    print('get_player_for_next_turn(): This function returns the player who makes a move:', active_player)
+    return active_player
+
+def сheck_if_player_loose():
+    print('сheck_if_player_loose(): This function checks if a player loses')
+
+def greetings_to_looser():
+    print('greetings_to_looser(): This function send greetings to a player looser!')
 
 while True:
-    def is_winner_defined():
-        print('is_winner_defined(): This function checks whether the winner is found')
     is_winner_defined()
-
-    def greetings_to_winner():
-        print('greetings _to_winner(): This function send greetings to the game winner!')
     greetings_to_winner()
-
-    def get_player_for_next_turn():
-        print('get_player_for_next_turn(): This function returns the player who makes a move:', player1)
-        return player1
-
     active_player = get_player_for_next_turn()
     active_player.position = gamer.Gamer.make_turn(active_player)
-
-    def сheck_if_player_loose():
-        print ('сheck_if_player_loose(): This function checks if a player loses')
     сheck_if_player_loose()
-
-    def greetings_to_looser():
-        print('greetings_to_looser(): This function send greetings to a player looser!')
     greetings_to_looser()
-
-    input ('Press button Enter to start a new loop iteration')
-
+    key = getch()
+    if key == b'\x1b':
+        break
+    if key == b'\r':
+        continue
